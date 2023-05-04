bbcc45d3b9da66b5b2e6a5133fda2b4d15f4d2ce
webster <mik.seynaroev@gmail.com>
2023-05-04 15:30:58 +0300

    UNET for flood area segmentation

---
+++ Unet/train.py
@@ -0,0 +1,131 @@
+import os
+
+import torch
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+from tqdm import tqdm
+import torch.nn as nn
+import torch.optim as optim
+from model import UNET
+from utils import (
+    load_checkpoint,
+    save_checkpoint,
+    get_loaders,
+    check_accuracy,
+    save_predictions_as_imgs
+)
+
+# Hyperparameters
+LEARNING_RATE = 1e-4
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+BATCH_SIZE = 16
+NUM_EPOCHS = 20
+NUM_WORKERS = os.cpu_count()
+IMAGE_HEIGHT = 256
+IMAGE_WIDTH = 256
+PIN_MEMORY = True
+LOAD_MODEL = False
+TRAIN_IMG_DIR = "data/train_images/"
+TRAIN_MASK_DIR = "data/train_masks/"
+VAL_IMG_DIR = "data/val_images/"
+VAL_MASK_DIR = "data/val_masks/"
+
+
+def train_fn(loader, model, optimizer, loss_fn, scaler):
+    loop = tqdm(loader)
+
+    for batch_size, (x, y) in enumerate(loop):
+        x = x.to(device=DEVICE)
+        y = y.float().unsqueeze(1).to(device=DEVICE)
+
+        with torch.cuda.amp.autocast():
+            logits = model(x)
+            loss = loss_fn(logits, y)
+
+        optimizer.zero_grad()
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        scaler.update()
+
+        loop.set_postfix(loss=loss.item())
+
+
+def main():
+    train_transform = A.Compose(
+        [
+            A.Resize(height=IMAGE_HEIGHT, width=IMAGE_WIDTH),
+            A.Rotate(limit=35, p=1.0),
+            A.HorizontalFlip(p=0.5),
+            # A.VerticalFlip(p=0.1),
+            A.Normalize(
+                mean=[0.0, 0.0, 0.0],
+                std=[1.0, 1.0, 1.0],
+                max_pixel_value=255.0
+            ),
+            ToTensorV2(),
+        ]
+    )
+
+    val_transform = A.Compose(
+        [
+            A.Resize(height=IMAGE_HEIGHT, width=IMAGE_WIDTH),
+            A.Normalize(
+                mean=[0.0, 0.0, 0.0],
+                std=[1.0, 1.0, 1.0],
+                max_pixel_value=255.0
+            ),
+            ToTensorV2(),
+        ]
+    )
+
+    model = UNET(in_channels=3, out_channels=1).to(DEVICE)
+    loss_fn = nn.BCEWithLogitsLoss()
+    optimizer = optim.Adam(params=model.parameters(), lr=LEARNING_RATE)
+
+    train_loader, val_loader = get_loaders(
+        train_dir=TRAIN_IMG_DIR,
+        train_maskdir=TRAIN_MASK_DIR,
+        val_dir=VAL_IMG_DIR,
+        val_maskdir=VAL_MASK_DIR,
+        batch_size=BATCH_SIZE,
+        train_transform=train_transform,
+        val_transform=val_transform,
+        num_workers=NUM_WORKERS,
+        pin_memory=PIN_MEMORY
+    )
+
+    if LOAD_MODEL:
+        load_checkpoint(checkpoint=torch.load("my_checkpoint.pth.tar"), model=model)
+
+    check_accuracy(loader=val_loader,
+                   model=model,
+                   device=DEVICE)
+
+    scaler = torch.cuda.amp.GradScaler()
+    dice_score = 0
+
+    for epoch in range(NUM_EPOCHS):
+        train_fn(loader=train_loader,
+                 model=model,
+                 loss_fn=loss_fn,
+                 optimizer=optimizer,
+                 scaler=scaler)
+        if dice_score > 0.8:
+            checkpoint = {
+                "state_dict": model.state_dict(),
+                "optimizer": optimizer.state_dict()
+            }
+            save_checkpoint(checkpoint)
+
+        dice_score = check_accuracy(loader=val_loader,
+                                    model=model,
+                                    device=DEVICE)
+
+        save_predictions_as_imgs(loader=val_loader,
+                                 model=model,
+                                 folder="saved_images/",
+                                 device="cuda")
+
+
+if __name__ == "__main__":
+    main()
